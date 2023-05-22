8eaf49f72264773220aabb9f6aae79b8a367ad54
Sreenivasan Naarayanan <sreenivasannaarayanan@Sreenivasans-MacBook-Pro.local>
2023-05-22 16:07:37 -0500

    fix: test nx affected

--- libs/reusable-lib/src/lib/reusable-lib.spec.ts
+++ libs/reusable-lib/src/lib/reusable-lib.spec.ts
@@ -2,6 +2,6 @@ import { reusableLib } from './reusable-lib';
 
 describe('reusableLib', () => {
   it('should work', () => {
-    expect(reusableLib()).toEqual('reusable-lib-1');
+    expect(reusableLib()).toEqual('reusable-lib-2');
   });
 });
