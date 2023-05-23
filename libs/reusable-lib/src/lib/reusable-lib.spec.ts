701a323734c7242d28fec590f492d899aae224f0
Sreenivasan Naarayanan <sreenivasannaarayanan@Sreenivasans-MacBook-Pro.local>
2023-05-22 23:32:16 -0500

    fix: bump reusable lib

--- libs/reusable-lib/src/lib/reusable-lib.spec.ts
+++ libs/reusable-lib/src/lib/reusable-lib.spec.ts
@@ -2,6 +2,6 @@ import { reusableLib } from './reusable-lib';
 
 describe('reusableLib', () => {
   it('should work', () => {
-    expect(reusableLib()).toEqual('reusable-lib-17');
+    expect(reusableLib()).toEqual('reusable-lib-18');
   });
 });
