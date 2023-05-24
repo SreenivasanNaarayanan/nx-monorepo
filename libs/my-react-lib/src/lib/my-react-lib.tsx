c5faf5c64bdc38ac49186d552b5fa73357c47b1e
Sreenivasan Naarayanan <sreenivasannaarayanan@Sreenivasans-MacBook-Pro.local>
2023-05-23 21:04:02 -0500

    fix: test pr

--- libs/my-react-lib/src/lib/my-react-lib.tsx
+++ libs/my-react-lib/src/lib/my-react-lib.tsx
@@ -7,7 +7,7 @@ export interface MyReactLibProps {}
 export function MyReactLib(props: MyReactLibProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to MyReactLib7!</h1>
+      <h1>Welcome to MyReactLib6!</h1>
       <h2>{reusableLib()}</h2>
     </div>
   );
